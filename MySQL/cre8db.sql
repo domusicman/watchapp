bd6ed153c3e2a647be6ab92fcb0394b2964a8b14
domusicman <dom@Refrigerator.local>
2019-09-22 18:10:18 -0700

    add gitignore, setup project up better, add function for fetching data and send to frontend

---
+++ MySQL/cre8db.sql
@@ -0,0 +1,5 @@
+Use appdb;
+
+Create table `watches` (id int, brand varchar(20));
+
+Insert into watches (id, brand) values (1, 'Patek Phillipe');
--- go/main.go
+++ go/main.go
@@ -3,45 +3,65 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"text/template"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-type watch struct {
-	id   int
-	name string
+type watchInfo struct {
+	ID    int
+	Brand string
 }
 
 var (
-	tpl      *template.Template
-	cnn, err = sql.Open("mysql", "root:root@tcp(db:3306)/watchesdb")
+	tpl *template.Template
 )
 
 func init() {
-	tpl = template.Must(template.ParseGlob("/go/photosite/templates/*"))
+	tpl = template.Must(template.ParseGlob("/go/templates/*"))
+}
 
 func main() {
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8080", nil)
 }
 
+//This function get watch id and brand using getWatch function and passes them to the gohtml template file
 func index(w http.ResponseWriter, r *http.Request) {
-	id := 1
-	var name string
+	watch, err := getWatch("1")
+	HandleError(w, err)
 
-	if err := cnn.QueryRow("SELECT name FROM watches WHERE id = ? LIMIT 1", id).Scan(&name); err != nil {
-		log.Fatal(err)
-	}
-
-	err := tpl.ExecuteTemplate(w, "pic.gohtml", id)
+	err = tpl.ExecuteTemplate(w, "pic.gohtml", watch)
 	HandleError(w, err)
 
 }
 
+//this is handling an error and can be called it in other page functions
 func HandleError(w http.ResponseWriter, err error) {
 	if err != nil {
 		fmt.Println("Index did not work. error in index")
 	}
+}
+
+//This function takes id # as input and outputs watchid and corresponding brand
+
+func getWatch(id string) (watch watchInfo, err error) {
+	cnn, err := sql.Open("mysql", "root:root@tcp(db:3306)/appdb")
+	defer cnn.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = cnn.Ping()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	err = cnn.QueryRow("SELECT id, brand from watches where id = ?;", id).Scan(&watch.ID, &watch.Brand)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return watch, err
 }
